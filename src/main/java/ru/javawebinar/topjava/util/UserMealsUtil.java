c177c631ef748e7a5cc89a090bf0c8a207b21402
Alexandr Nevzorov <nevz2293@gmail.com>
2017-10-18 17:52:04 +0400

    HW0 (1 version)

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -3,9 +3,11 @@ package ru.javawebinar.topjava.util;
 import ru.javawebinar.topjava.model.UserMeal;
 import ru.javawebinar.topjava.model.UserMealWithExceed;
 
+import java.sql.Time;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.Month;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -23,13 +25,35 @@ public class UserMealsUtil {
                 new UserMeal(LocalDateTime.of(2015, Month.MAY, 31,13,0), "Обед", 500),
                 new UserMeal(LocalDateTime.of(2015, Month.MAY, 31,20,0), "Ужин", 510)
         );
-        getFilteredWithExceeded(mealList, LocalTime.of(7, 0), LocalTime.of(12,0), 2000);
+        List<UserMealWithExceed> mealWithExceedList= getFilteredWithExceeded(mealList, LocalTime.of(7, 0), LocalTime.of(21,0), 2000);
+        for (UserMealWithExceed u : mealWithExceedList){
+            System.out.println(u);
+        }
 //        .toLocalDate();
 //        .toLocalTime();
     }
 
     public static List<UserMealWithExceed>  getFilteredWithExceeded(List<UserMeal> mealList, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
-        System.out.println("TODO return filtered list with correctly exceeded field");
-        return null;
+        List<UserMealWithExceed> mealWithExceedList = new ArrayList<>();
+        int day=0;
+        int coloies=0;
+        for(int i =0;i<mealList.size();i++){
+            coloies+=mealList.get(i).getCalories();
+            day = mealList.get(i).getDateTime().getDayOfYear();
+
+            System.out.println(day);
+            if (TimeUtil.isBetween(LocalTime.from(mealList.get(i).getDateTime()),startTime,endTime))
+                mealWithExceedList.add(new UserMealWithExceed(mealList.get(i).getDateTime(), mealList.get(i).getDescription(), mealList.get(i).getCalories(), coloies>caloriesPerDay));
+            try {
+                if (mealList.get(i + 1).getDateTime().getDayOfYear() != day) {
+                    coloies = 0;
+                }
+            }
+            catch (ArrayIndexOutOfBoundsException e){
+                System.out.println("Exception e");
+            }
+        }
+        //System.out.println("TODO return filtered list with correctly exceeded field");
+        return mealWithExceedList;
     }
 }
