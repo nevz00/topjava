0bec791de467d2a4a244a87e244d9adc4aa0e3e6
Alexandr Nevzorov <nevz2293@gmail.com>
2017-11-22 18:47:34 +0400

    HW2 dz

--- src/main/java/ru/javawebinar/topjava/repository/mock/InMemoryMealRepositoryImpl.java
+++ src/main/java/ru/javawebinar/topjava/repository/mock/InMemoryMealRepositoryImpl.java
@@ -3,49 +3,76 @@ package ru.javawebinar.topjava.repository.mock;
 import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
+import ru.javawebinar.topjava.util.DateTimeUtil;
 import ru.javawebinar.topjava.util.MealsUtil;
 
+import java.time.LocalDateTime;
+import java.time.Month;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static ru.javawebinar.topjava.repository.mock.InMemoryUserRepositoryImpl.ADMIN_ID;
+import static ru.javawebinar.topjava.repository.mock.InMemoryUserRepositoryImpl.USER_ID;
 
 @Repository
 public class InMemoryMealRepositoryImpl implements MealRepository {
-    private Map<Integer, Meal> repository = new ConcurrentHashMap<>();
+    private Map<Integer, Map<Integer, Meal>> repository = new ConcurrentHashMap<>();
     private AtomicInteger counter = new AtomicInteger(0);
 
     {
-        MealsUtil.MEALS.forEach(this::save);
+        MealsUtil.MEALS.forEach(meal -> save(meal, USER_ID));
+
+        save(new Meal(LocalDateTime.of(2015, Month.JUNE, 1, 14, 0), "Админ ланч", 510), ADMIN_ID);
+        save(new Meal(LocalDateTime.of(2015, Month.JUNE, 1, 21, 0), "Админ ужин", 1500), ADMIN_ID);
     }
 
     @Override
-    public Meal save(Meal meal) {
+    public Meal save(Meal meal, int userId) {
+        Map<Integer, Meal> meals = repository.computeIfAbsent(userId,ConcurrentHashMap::new);
         if (meal.isNew()) {
             meal.setId(counter.incrementAndGet());
+            meals.put(meal.getId(),meal);
+            return meal;
         }
-        repository.put(meal.getId(), meal);
-        return meal;
+        return meals.computeIfPresent(meal.getId(),(id, oldMeal) -> meal);
     }
 
     @Override
-    public void delete(int id) {
-        repository.remove(id);
+    public boolean delete(int id, int userId) {
+        Map<Integer, Meal> meals = repository.get(userId);
+        return meals.remove(id)!=null;
     }
 
     @Override
-    public Meal get(int id) {
-        if(!repository.containsKey(id))
-            return null;
-        return repository.get(id);
+    public Meal get(int id, int userId) {
+        Map<Integer, Meal> meals = repository.get(userId);
+        return meals==null? null : meals.get(id);
     }
 
     @Override
-    public Collection<Meal> getAll() {
-        Collection<Meal> meals = repository.values();
+    public Collection<Meal> getAll(int userId) {
+        Map<Integer, Meal> meal = repository.get(userId);
+        Collection<Meal> meals = meal.values();
         List<Meal> list = meals.stream().sorted(((o1, o2) -> o2.getDateTime().compareTo(o1.getDateTime()))).collect(Collectors.toList());
         meals = list;
         return meals;
+    }
+
+    @Override
+    public List<Meal> getBetween(LocalDateTime startDateTime, LocalDateTime endDateTime, int userId) {
+        return getAllAsStream(userId)
+                .filter(meal -> DateTimeUtil.isBetween(meal.getDateTime(),startDateTime,endDateTime))
+                .collect(Collectors.toList());
+    }
+    private Stream<Meal> getAllAsStream(int userId) {
+        Map<Integer, Meal> meals = repository.get(userId);
+        return meals == null ?
+                Stream.empty() :
+                meals.values().stream()
+                        .sorted(Comparator.comparing(Meal::getDateTime).reversed());
     }
 }
 
--- src/main/java/ru/javawebinar/topjava/service/MealServiceImpl.java
+++ src/main/java/ru/javawebinar/topjava/service/MealServiceImpl.java
@@ -1,9 +1,50 @@
 package ru.javawebinar.topjava.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static ru.javawebinar.topjava.util.ValidationUtil.checkNotFoundWithId;
 
 public class MealServiceImpl implements MealService {
 
     private MealRepository repository;
 
+    @Autowired
+    public MealServiceImpl(MealRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public Meal get(int id, int userId) {
+        return checkNotFoundWithId(repository.get(id,userId),id);
+    }
+
+    @Override
+    public void delete(int id, int userId) {
+        checkNotFoundWithId(repository.delete(id,userId),id);
+    }
+
+    @Override
+    public List<Meal> getAll(int userId) {
+        return (List<Meal>) repository.getAll(userId);
+    }
+
+    @Override
+    public Meal update(Meal meal, int userId) {
+        return checkNotFoundWithId(repository.save(meal,userId),meal.getId());
+    }
+
+    @Override
+    public Meal create(Meal meal, int userId) {
+        return repository.save(meal,userId);
+    }
+
+    @Override
+    public List<Meal> getBetweenDateTimes(LocalDateTime startDateTime, LocalDateTime endDateTime, int userId) {
+        return repository.getBetween(startDateTime,endDateTime,userId);
+    }
 }
