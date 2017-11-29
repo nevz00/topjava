1f2a2aa8de9e02215c393d09ce927776dd4e1e25
Alexandr Nevzorov <nevz2293@gmail.com>
2017-11-29 17:56:27 +0400

    HW03 version 1

---
+++ src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
@@ -0,0 +1,36 @@
+package ru.javawebinar.topjava.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MealServiceTest {
+    @Test
+    public void get() throws Exception {
+    }
+
+    @Test
+    public void delete() throws Exception {
+    }
+
+    @Test
+    public void getBetweenDates() throws Exception {
+    }
+
+    @Test
+    public void getBetweenDateTimes() throws Exception {
+    }
+
+    @Test
+    public void getAll() throws Exception {
+    }
+
+    @Test
+    public void update() throws Exception {
+    }
+
+    @Test
+    public void create() throws Exception {
+    }
+
+}
