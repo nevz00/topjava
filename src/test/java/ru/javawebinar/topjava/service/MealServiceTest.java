2c4aae837bb88d087effb81a0f558c8a2b409b02
Alexandr Nevzorov <nevz2293@gmail.com>
2017-11-30 15:41:23 +0400

    HW03 version 2

--- src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
@@ -1,10 +1,32 @@
 package ru.javawebinar.topjava.service;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.bridge.SLF4JBridgeHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlConfig;
+import org.springframework.test.context.junit4.SpringRunner;
+import ru.javawebinar.topjava.model.Meal;
 
 import static org.junit.Assert.*;
 
+@ContextConfiguration({
+        "classpath:spring/spring-app.xml",
+        "classpath:spring/spring-db.xml"
+})
+@RunWith(SpringRunner.class)
+@Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
+
 public class MealServiceTest {
+
+    static {
+        SLF4JBridgeHandler.install();
+    }
+    @Autowired
+    MealService mealService;
+
     @Test
     public void get() throws Exception {
     }
