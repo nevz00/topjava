f1699c70bd30573a437fa44ee33189900006764f
Alexandr Nevzorov <nevz2293@gmail.com>
2018-02-27 15:39:05 +0400

    HW07 add meal rest controller test(again)

---
+++ src/test/java/ru/javawebinar/topjava/web/meal/MealRestControllerTest.java
@@ -0,0 +1,75 @@
+package ru.javawebinar.topjava.web.meal;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.ResultActions;
+import ru.javawebinar.topjava.TestUtil;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.service.MealService;
+import ru.javawebinar.topjava.util.MealsUtil;
+import ru.javawebinar.topjava.web.AbstractControllerTest;
+import ru.javawebinar.topjava.web.json.JsonUtil;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static ru.javawebinar.topjava.MealTestData.*;
+import static ru.javawebinar.topjava.TestUtil.contentJson;
+import static ru.javawebinar.topjava.UserTestData.USER;
+import static ru.javawebinar.topjava.UserTestData.USER_ID;
+import static ru.javawebinar.topjava.model.AbstractBaseEntity.START_SEQ;
+
+public class MealRestControllerTest extends AbstractControllerTest {
+    private static final String REST_URL = MealRestController.REST_URL + '/';
+
+    @Autowired
+    private MealService service;
+
+    @Test
+    public void testGet() throws Exception {
+        mockMvc.perform(get(REST_URL+MEAL1_ID))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(contentJson(MEAL1));
+    }
+    @Test
+    public void testDelete() throws Exception{
+        mockMvc.perform(delete(REST_URL+MEAL1_ID))
+                .andExpect(status().isOk())
+                .andDo(print());
+        assertMatch(service.getAll(START_SEQ), MEAL6,MEAL5,MEAL4,MEAL3,MEAL2);
+    }
+    @Test
+    public void testUpdate() throws Exception{
+        Meal updated = getUpdated();
+        mockMvc.perform(put(REST_URL+MEAL1_ID).contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(JsonUtil.writeValue(updated)))
+                .andExpect(status().isOk());
+        assertMatch(service.get(MEAL1_ID,START_SEQ), updated);
+    }
+
+    @Test
+    public void testCreate() throws Exception{
+        Meal created = getCreated();
+        ResultActions action = mockMvc.perform(post(REST_URL)
+        .contentType(MediaType.APPLICATION_JSON_VALUE)
+        .content(JsonUtil.writeValue(created)))
+                .andExpect(status().isCreated());
+        Meal returned = TestUtil.readFromJson(action, Meal.class);
+        created.setId(returned.getId());
+        assertMatch(returned, created);
+        assertMatch(service.getAll(START_SEQ),created,MEAL6,MEAL5,MEAL4,MEAL3,MEAL2,MEAL1);
+    }
+
+    @Test
+    public void testGetAll() throws Exception{
+        mockMvc.perform(get(REST_URL))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(contentJson(MealsUtil.getWithExceeded(MEALS, USER.getCaloriesPerDay())));
+    }
+}
